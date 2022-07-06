ec92717d5cdf866dfad03d68d4d1fce16ad0d796
hongsub11 <105191939+hongsub11@users.noreply.github.com>
2022-07-06 14:50:33 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// @prepros-append vendor/jquery-3.3.1.min.js
+// @prepros-append vendor/popper.min.js
+// @prepros-append vendor/bootstrap.min.js
+// @prepros-append vendor/owl.carousel.min.js
+// @prepros-append vendor/aos.js
+// @prepros-append vendor/isotope.pkgd.min.js
+// @prepros-append vendor/jquery.animateNumber.min.js
+// @prepros-append vendor/jquery.waypoints.min.js
+// @prepros-append vendor/TweenMax.min.js
+// @prepros-append vendor/jquery.easing.1.3.js
+// @prepros-append vendor/jarallax.min.js
+// @prepros-append vendor/jarallax-video.min.js
+// @prepros-append vendor/jquery.validate.min.js
+// @prepros-append vendor/jquery.fancybox.min.js
+// @prepros-append vendor/stickyfill.min.js
+// @prepros-append vendor/imagesloaded.pkgd.min.js
+// @prepros-append vendor/ScrollMagic.min.js
+// @prepros-append vendor/scrollmagic.animation.gsap.min.js
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {//아이콘이 나타나길 원하는 높이를 설정하세요
+        $('.top').fadeIn();//나타날 아이콘 클래스 수정!
+    } else {
+        $('.top').fadeOut();//나타날 아이콘 클래스 수정!
+    }
+});
